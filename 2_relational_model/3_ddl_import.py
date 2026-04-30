43ae7b92e1b6c7b318f4bd769e6178eef14e7761
JT07616 <juraj.tojcic@student.unipu.hr>
2026-04-30 23:44:15 +0200

    Initial commit with 3 checpoints

---
+++ 2_relational_model/3_ddl_import.py
@@ -0,0 +1,334 @@
+# Imports
+import pandas as pd
+import json
+import requests
+import random
+from sqlalchemy import Date
+from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey, insert
+from sqlalchemy.orm import sessionmaker, declarative_base
+from typing import List, Dict, Any
+
+# Putanja do predprocesirane CSV datoteke
+CSV_FILE_PATH = "atp_matches_processed_80.csv"
+
+# Učitavanje CSV datoteke u dataframe
+df = pd.read_csv(CSV_FILE_PATH, delimiter=',')
+print(f"CSV size: {df.shape}")  # Print dataset size
+print(df.head())  # Preview first few rows
+
+# Database Connection
+Base = declarative_base()
+
+# Definiranje sheme baze podataka
+# --------------------------------------------------------------
+class Country(Base):
+    __tablename__ = 'country'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(45), nullable=False, unique=True)
+    population = Column(Integer, nullable=False)
+    region = Column(String(45), nullable=False)
+
+class Tournament(Base):
+    __tablename__ = 'tournament'
+
+    id = Column(Integer, primary_key=True)  # tourney_id
+    name = Column(String(100), nullable=False)
+    surface = Column(String(20))
+    draw_size = Column(Integer)
+    level = Column(String(10))
+    start_date = Column(Date)
+    city = Column(String(50))
+    country_fk = Column(Integer, ForeignKey('country.id'))
+
+
+class Player(Base):
+    __tablename__ = 'player'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    hand = Column(String(5))
+    height = Column(Integer)
+    country_fk = Column(Integer, ForeignKey('country.id'))
+
+class Match(Base):
+    __tablename__ = 'match'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    tournament_fk = Column(Integer, ForeignKey('tournament.id'))
+    match_num = Column(Integer)
+    score = Column(String(100))
+    best_of = Column(Integer)
+    round = Column(String(20))
+    minutes = Column(Integer)
+    winner_fk = Column(Integer, ForeignKey('player.id'))
+    loser_fk = Column(Integer, ForeignKey('player.id'))
+
+
+class PlayerMatchStats(Base):
+    __tablename__ = 'player_match_stats'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    match_fk = Column(Integer, ForeignKey('match.id'))
+    player_fk = Column(Integer, ForeignKey('player.id'))
+    is_winner = Column(Integer, nullable=False) // u tablicu je dodana mjera iswinner koja oznacava pobjenika/gubitnika.
+    seed = Column(String(10))
+    entry = Column(String(10))
+    rank = Column(Integer)
+    rank_points = Column(Integer)
+    ace = Column(Integer)
+    double_fault = Column(Integer)
+    service_points = Column(Integer)
+    first_in = Column(Integer)
+    first_won = Column(Integer)
+    second_won = Column(Integer)
+    service_games = Column(Integer)
+    break_points_saved = Column(Integer)
+    break_points_faced = Column(Integer)
+
+
+
+# Database Connection
+
+print("="*50)
+print("KORAK 1: Spajam se na bazu...")
+
+engine = create_engine('mysql+pymysql://root:root@localhost:3306/tenis', echo=False)
+
+print("KORAK 2: Brišem postojeće tablice...")
+Base.metadata.drop_all(engine)
+
+print("KORAK 3: Kreiram nove tablice...")
+Base.metadata.create_all(engine)
+
+print("KORAK 4: Tablice kreirane, provjera...")
+from sqlalchemy import inspect
+inspector = inspect(engine)
+tables = inspector.get_table_names()
+print(f"Tablice u bazi NAKON kreiranja: {tables}")
+print("="*50)
+
+Session = sessionmaker(bind=engine) # Stvaranje sesije
+session = Session() # Otvori novu sesiju
+
+# --------------------------------------------------------------
+# Import podataka
+# ------------------------------------------------------------
+
+# **1. Umetanje zemalja**
+countries = pd.concat([
+    df[['tourney_country']].rename(columns={'tourney_country': 'name'}),
+    df[['winner_country']].rename(columns={'winner_country': 'name'}),
+    df[['loser_country']].rename(columns={'loser_country': 'name'})
+]).drop_duplicates().dropna()
+
+countries_list = []
+
+for idx, (i, row) in enumerate(countries.iterrows(), 1):
+
+    original_name = row['name']
+
+    try:
+        response = requests.get(f"https://restcountries.com/v3.1/name/{original_name}?fullText=true")
+        data = json.loads(response.content)
+
+        if isinstance(data, list) and len(data) > 0:
+            country_entry = {
+                "id": idx,
+                "name": original_name,
+                "population": data[0].get('population', 0),
+                "region": data[0].get('region', 'Unknown')
+            }
+        else:
+            raise Exception("Nema podataka")
+
+    except:
+        print(f"⚠ Problem s državom: {original_name}")
+
+        country_entry = {
+            "id": idx,
+            "name": original_name,
+            "population": 0,
+            "region": "Unknown"
+        }
+
+    countries_list.append(country_entry)
+
+session.execute(insert(Country), countries_list)  # Bulk insert
+session.commit()
+
+print("Countries inserted!")
+
+country_map = {c.name: c.id for c in session.query(Country).all()}
+
+
+# **2. Umetanje turnira**
+tournaments = df[['tourney_id', 'tourney_name', 'surface', 'draw_size',
+                  'tourney_level', 'tourney_date', 'tourney_city', 'tourney_country']].drop_duplicates().copy()
+
+# Pretvori tourney_date u datum
+tournaments['tourney_date'] = pd.to_datetime(tournaments['tourney_date']).dt.date
+
+# Mapiranje države iz teksta u ID
+tournaments['country_fk'] = tournaments['tourney_country'].map(country_map)
+
+# Preimenovanje stupaca da odgovaraju tablici
+tournaments = tournaments.rename(columns={
+    'tourney_id': 'id',
+    'tourney_name': 'name',
+    'tourney_level': 'level',
+    'tourney_date': 'start_date',
+    'tourney_city': 'city'
+})
+
+# Izbacujemo originalni tekstualni stupac države jer sada imamo country_fk
+tournaments = tournaments.drop(columns=['tourney_country'])
+
+# Pretvori u listu rječnika sa string ključevima
+tournaments_list = [{str(k): v for k, v in row.items()} for row in tournaments.to_dict(orient="records")]
+
+# Bulk insert
+session.execute(insert(Tournament), tournaments_list)
+session.commit()
+
+print("Tournaments inserted!")
+
+tournament_map = {t.id: t.id for t in session.query(Tournament).all()}
+# Vrijednost mapiranja: {tourney_id: tourney_id}
+
+
+# **3. Umetanje igrača**
+
+# WINNER dio
+players_winner = df[['winner_id', 'winner_name', 'winner_hand', 'winner_ht', 'winner_country']].copy()
+
+players_winner = players_winner.rename(columns={
+    'winner_id': 'id',
+    'winner_name': 'name',
+    'winner_hand': 'hand',
+    'winner_ht': 'height',
+    'winner_country': 'country'
+})
+
+# LOSER dio
+players_loser = df[['loser_id', 'loser_name', 'loser_hand', 'loser_ht', 'loser_country']].copy()
+
+players_loser = players_loser.rename(columns={
+    'loser_id': 'id',
+    'loser_name': 'name',
+    'loser_hand': 'hand',
+    'loser_ht': 'height',
+    'loser_country': 'country'
+})
+
+# SPOJI winner + loser
+players = pd.concat([players_winner, players_loser])
+
+# Makni duplikate (ključ = id)
+players = players.drop_duplicates(subset=['id'])
+
+# Mapiranje države
+players['country_fk'] = players['country'].map(country_map)
+
+# Makni tekstualni country
+players = players.drop(columns=['country'])
+
+# Pretvori u listu dict-ova
+players_list = [{str(k): v for k, v in row.items()} for row in players.to_dict(orient="records")]
+
+# Insert
+session.execute(insert(Player), players_list)
+session.commit()
+
+print("Players inserted!")
+
+# Map (trebat će kasnije)
+player_map = {p.id: p.id for p in session.query(Player).all()}
+
+
+# **4. Umetanje mečeva**
+matches = df[['tourney_id', 'match_num', 'score', 'best_of', 'round', 'minutes',
+              'winner_id', 'loser_id']].copy()
+
+# Preimenovanje stupaca da odgovaraju tablici
+matches = matches.rename(columns={
+    'tourney_id': 'tournament_fk',
+    'winner_id': 'winner_fk',
+    'loser_id': 'loser_fk'
+})
+
+# Pretvori u listu rječnika sa string ključevima
+matches_list = [{str(k): v for k, v in row.items()} for row in matches.to_dict(orient="records")]
+
+# Bulk insert
+session.execute(insert(Match), matches_list)
+session.commit()
+
+print("Matches inserted!")
+
+# **5. Umetanje player_match_stats**
+
+stats_list = []
+
+for idx, row in df.iterrows():
+    match_id = idx + 1  # jer match.id je autoincrement i ide redom
+
+    # ------------------
+    # WINNER
+    # ------------------
+    winner_stats = {
+        "match_fk": match_id,
+        "player_fk": row['winner_id'],
+        "is_winner": 1,
+
+        "seed": row['winner_seed'],
+        "entry": row['winner_entry'],
+        "rank": row['winner_rank'],
+        "rank_points": row['winner_rank_points'],
+
+        "ace": row['w_ace'],
+        "double_fault": row['w_df'],
+        "service_points": row['w_svpt'],
+        "first_in": row['w_1stIn'],
+        "first_won": row['w_1stWon'],
+        "second_won": row['w_2ndWon'],
+        "service_games": row['w_SvGms'],
+        "break_points_saved": row['w_bpSaved'],
+        "break_points_faced": row['w_bpFaced']
+    }
+
+    stats_list.append(winner_stats)
+
+    # ------------------
+    # LOSER
+    # ------------------
+    loser_stats = {
+        "match_fk": match_id,
+        "player_fk": row['loser_id'],
+        "is_winner": 0,
+
+        "seed": row['loser_seed'],
+        "entry": row['loser_entry'],
+        "rank": row['loser_rank'],
+        "rank_points": row['loser_rank_points'],
+
+        "ace": row['l_ace'],
+        "double_fault": row['l_df'],
+        "service_points": row['l_svpt'],
+        "first_in": row['l_1stIn'],
+        "first_won": row['l_1stWon'],
+        "second_won": row['l_2ndWon'],
+        "service_games": row['l_SvGms'],
+        "break_points_saved": row['l_bpSaved'],
+        "break_points_faced": row['l_bpFaced']
+    }
+
+    stats_list.append(loser_stats)
+
+# Insert
+session.execute(insert(PlayerMatchStats), stats_list)
+session.commit()
+
+print("PlayerMatchStats inserted!")
+
+print("Data imported successfully!")
---
+++ 3_dimensional_model/create_dimensional_model.py
@@ -0,0 +1,393 @@
+'''
+Skripta za generiranje dimenzijskog modela podataka -> star schema
+'''
+
+import pandas as pd
+from sqlalchemy import create_engine, Column, Integer, BigInteger, String, DateTime, Date, ForeignKey
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import text
+
+# Konekcija na bazu
+DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/tenis"
+
+engine = create_engine(DATABASE_URL, echo=False)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+
+# ========== DEFINICIJA DIMENZIJSKIH I FACT TABLICA ==========
+
+class DimPlayer(Base):
+    __tablename__ = 'dim_player'
+
+    player_tk = Column(BigInteger, primary_key=True, autoincrement=True)
+
+    player_id = Column(Integer, index=True)
+    name = Column(String(100))
+    hand = Column(String(5))
+    height = Column(Integer)
+    country_name = Column(String(45))
+    region = Column(String(45))
+
+# Hijerarhije:
+# 1. Lokacijska: region -> country_name -> city -> name
+# 2. Turnirska: level -> name
+class DimTournament(Base):
+    __tablename__ = 'dim_tournament'
+
+    tournament_tk = Column(BigInteger, primary_key=True, autoincrement=True)
+
+    tournament_id = Column(Integer, index=True)
+    name = Column(String(100))
+    surface = Column(String(20))
+    draw_size = Column(Integer)
+    level = Column(String(10))
+    city = Column(String(50))
+    country_name = Column(String(45))
+    region = Column(String(45))
+
+
+class DimCountry(Base):
+    __tablename__ = 'dim_country'
+
+    country_tk = Column(BigInteger, primary_key=True, autoincrement=True)
+
+    country_id = Column(Integer, index=True)
+    name = Column(String(45))
+    population = Column(Integer)
+    region = Column(String(45))
+
+# Hijerarhija: year -> quarter -> month -> day
+class DimDate(Base):
+    __tablename__ = 'dim_date'
+
+    date_tk = Column(Integer, primary_key=True, autoincrement=True)
+    date = Column(Date, nullable=False)
+    day = Column(Integer, nullable=False)
+    month = Column(Integer, nullable=False)
+    quarter = Column(Integer, nullable=False)
+    year = Column(Integer, nullable=False)
+
+# Hijerarhija: round -> match
+# round predstavlja fazu turnira (R32, R16, QF, SF, F)
+class DimMatchInfo(Base):
+    __tablename__ = 'dim_match_info'
+
+    match_info_tk = Column(BigInteger, primary_key=True, autoincrement=True)
+
+    score = Column(String(100))
+    best_of = Column(Integer)
+    round = Column(String(20))
+    seed = Column(String(10))
+    entry = Column(String(10))
+    rank = Column(Integer)
+
+
+class FactPlayerMatch(Base):
+    __tablename__ = 'fact_player_match'
+
+    fact_player_match_tk = Column(BigInteger, primary_key=True, autoincrement=True)
+
+    date_tk = Column(Integer, ForeignKey('dim_date.date_tk'))
+    player_tk = Column(BigInteger, ForeignKey('dim_player.player_tk'))
+    opponent_player_tk = Column(BigInteger, ForeignKey('dim_player.player_tk'))
+    tournament_tk = Column(BigInteger, ForeignKey('dim_tournament.tournament_tk'))
+    country_tk = Column(BigInteger, ForeignKey('dim_country.country_tk'))
+    match_info_tk = Column(BigInteger, ForeignKey('dim_match_info.match_info_tk'))
+
+    match_num = Column(Integer) # degenerirana dimenzija
+     
+    #mjere
+    ace = Column(Integer)
+    double_fault = Column(Integer)
+    service_points = Column(Integer)
+    first_in = Column(Integer)
+    first_won = Column(Integer)
+    second_won = Column(Integer)
+    service_games = Column(Integer)
+    break_points_saved = Column(Integer)
+    break_points_faced = Column(Integer)
+    rank_points = Column(Integer)
+    minutes = Column(Integer)
+
+
+# ========== TESTIRANJE KONEKCIJE ==========
+
+print("Testiram konekciju...")
+with engine.connect() as conn:
+    conn.execute(text("SELECT 1"))
+    print("Konekcija uspješna!")
+
+
+# ========== BRISANJE STARIH DIMENZIJSKIH TABLICA ==========
+
+print("\nBrišem postojeće dimenzijske tablice...")
+
+try:
+    FactPlayerMatch.__table__.drop(engine, checkfirst=True)
+    DimMatchInfo.__table__.drop(engine, checkfirst=True)
+    DimDate.__table__.drop(engine, checkfirst=True)
+    DimCountry.__table__.drop(engine, checkfirst=True)
+    DimTournament.__table__.drop(engine, checkfirst=True)
+    DimPlayer.__table__.drop(engine, checkfirst=True)
+    print("Postojeće dimenzijske tablice obrisane.")
+except:
+    print("Nema postojećih dimenzijskih tablica.")
+
+
+# ========== KREIRANJE TABLICA ==========
+
+print("\nKreiram dimenzijske i fact tablice...")
+Base.metadata.create_all(engine)
+print("Tablice kreirane!")
+
+
+# ========== ETL PROCES ==========
+
+print("\nPopunjavam dimenzijske tablice...")
+
+
+# 1. dim_player
+print("Popunjavam dim_player...")
+
+players_query = """
+SELECT DISTINCT
+    p.id AS player_id,
+    p.name AS name,
+    p.hand AS hand,
+    p.height AS height,
+    c.name AS country_name,
+    c.region AS region
+FROM player p
+LEFT JOIN country c ON p.country_fk = c.id;
+"""
+
+df_players = pd.read_sql(players_query, engine)
+
+
+df_players.to_sql('dim_player', engine, if_exists='append', index=False)
+print(f"Uneseno {len(df_players)} redaka u dim_player")
+
+
+# 2. dim_tournament
+print("Popunjavam dim_tournament...")
+
+tournaments_query = """
+SELECT DISTINCT
+    t.id AS tournament_id,
+    t.name AS name,
+    t.surface AS surface,
+    t.draw_size AS draw_size,
+    t.level AS level,
+    t.city AS city,
+    c.name AS country_name,
+    c.region AS region
+FROM tournament t
+LEFT JOIN country c ON t.country_fk = c.id;
+"""
+
+df_tournaments = pd.read_sql(tournaments_query, engine)
+df_tournaments.to_sql('dim_tournament', engine, if_exists='append', index=False)
+print(f"Uneseno {len(df_tournaments)} redaka u dim_tournament")
+
+
+# 3. dim_country
+print("Popunjavam dim_country...")
+
+countries_query = """
+SELECT DISTINCT
+    c.id AS country_id,
+    c.name AS name,
+    c.population AS population,
+    c.region AS region
+FROM country c;
+"""
+
+df_countries = pd.read_sql(countries_query, engine)
+df_countries.to_sql('dim_country', engine, if_exists='append', index=False)
+print(f"Uneseno {len(df_countries)} redaka u dim_country")
+
+
+# 4. dim_date
+print("Popunjavam dim_date...")
+
+dates_query = """
+SELECT DISTINCT
+    start_date
+FROM tournament
+WHERE start_date IS NOT NULL
+ORDER BY start_date;
+"""
+
+df_dates = pd.read_sql(dates_query, engine)
+df_dates['date'] = pd.to_datetime(df_dates['start_date'])
+
+df_dates_final = pd.DataFrame({
+    'date': df_dates['date'].dt.date,
+    'day': df_dates['date'].dt.day,
+    'month': df_dates['date'].dt.month,
+    'quarter': df_dates['date'].dt.quarter,
+    'year': df_dates['date'].dt.year
+})
+
+df_dates_final.to_sql('dim_date', engine, if_exists='append', index=False)
+print(f"Uneseno {len(df_dates_final)} redaka u dim_date")
+
+
+# 5. dim_match_info
+print("Popunjavam dim_match_info...")
+
+match_info_query = """
+SELECT DISTINCT
+    m.score AS score,
+    m.best_of AS best_of,
+    m.round AS round,
+    pms.seed AS seed,
+    pms.entry AS entry,
+    pms.`rank` AS `rank`
+FROM player_match_stats pms
+INNER JOIN `match` m ON pms.match_fk = m.id;
+"""
+
+df_match_info = pd.read_sql(match_info_query, engine)
+df_match_info.to_sql('dim_match_info', engine, if_exists='append', index=False)
+print(f"Uneseno {len(df_match_info)} redaka u dim_match_info")
+
+
+# ========== PUNJENJE FACT TABLICE ==========
+
+print("\nPopunjavam fact_player_match...")
+
+fact_query = """
+INSERT INTO fact_player_match (
+    date_tk,
+    player_tk,
+    opponent_player_tk,
+    tournament_tk,
+    country_tk,
+    match_info_tk,
+    match_num,
+    ace,
+    double_fault,
+    service_points,
+    first_in,
+    first_won,
+    second_won,
+    service_games,
+    break_points_saved,
+    break_points_faced,
+    rank_points,
+    minutes
+)
+SELECT
+    dd.date_tk,
+    dp.player_tk,
+    dop.player_tk AS opponent_player_tk,
+    dt.tournament_tk,
+    dc.country_tk,
+    dmi.match_info_tk,
+    m.match_num,
+    pms.ace,
+    pms.double_fault,
+    pms.service_points,
+    pms.first_in,
+    pms.first_won,
+    pms.second_won,
+    pms.service_games,
+    pms.break_points_saved,
+    pms.break_points_faced,
+    pms.rank_points,
+    m.minutes
+FROM player_match_stats pms
+INNER JOIN `match` m ON pms.match_fk = m.id
+INNER JOIN tournament t ON m.tournament_fk = t.id
+INNER JOIN player p ON pms.player_fk = p.id
+LEFT JOIN country c ON p.country_fk = c.id
+
+INNER JOIN dim_player dp
+    ON p.id = dp.player_id
+
+INNER JOIN dim_player dop
+    ON (
+        CASE
+            WHEN pms.player_fk = m.winner_fk THEN m.loser_fk
+            ELSE m.winner_fk
+        END
+    ) = dop.player_id
+
+INNER JOIN dim_tournament dt
+    ON t.id = dt.tournament_id
+
+INNER JOIN dim_country dc
+    ON c.id = dc.country_id
+
+INNER JOIN dim_date dd
+    ON t.start_date = dd.date
+
+INNER JOIN dim_match_info dmi
+    ON IFNULL(m.score, '') = IFNULL(dmi.score, '')
+    AND IFNULL(m.best_of, -1) = IFNULL(dmi.best_of, -1)
+    AND IFNULL(m.round, '') = IFNULL(dmi.round, '')
+    AND IFNULL(pms.seed, '') = IFNULL(dmi.seed, '')
+    AND IFNULL(pms.entry, '') = IFNULL(dmi.entry, '')
+    AND IFNULL(pms.`rank`, -1) = IFNULL(dmi.`rank`, -1);
+"""
+
+with engine.connect() as conn:
+    result = conn.execute(text(fact_query))
+    conn.commit()
+    print(f"Uneseno {result.rowcount} redaka u fact_player_match")
+
+
+# ========== VERIFIKACIJA ==========
+
+print("\nBroj redaka po tablicama:")
+
+tables = [
+    'dim_player',
+    'dim_tournament',
+    'dim_country',
+    'dim_date',
+    'dim_match_info',
+    'fact_player_match'
+]
+
+with engine.connect() as conn:
+    for table in tables:
+        result = conn.execute(text(f"SELECT COUNT(*) FROM {table}"))
+        count = result.scalar()
+        print(f"{table}: {count} redaka")
+
+
+print("\nUzorak podataka iz fact_player_match:")
+
+preview_query = """
+SELECT
+    p.name AS player,
+    op.name AS opponent,
+    t.name AS tournament,
+    c.name AS country,
+    d.year,
+    d.month,
+    mi.round,
+    mi.best_of,
+    f.match_num,
+    f.ace,
+    f.double_fault,
+    f.rank_points,
+    f.minutes
+FROM fact_player_match f
+INNER JOIN dim_player p ON f.player_tk = p.player_tk
+INNER JOIN dim_player op ON f.opponent_player_tk = op.player_tk
+INNER JOIN dim_tournament t ON f.tournament_tk = t.tournament_tk
+INNER JOIN dim_country c ON f.country_tk = c.country_tk
+INNER JOIN dim_date d ON f.date_tk = d.date_tk
+INNER JOIN dim_match_info mi ON f.match_info_tk = mi.match_info_tk
+LIMIT 5;
+"""
+
+df_preview = pd.read_sql(preview_query, engine)
+print(df_preview.to_string(index=False))
+
+print("\nDIMENZIJSKI MODEL USPJEŠNO KREIRAN I POPUNJEN!")
